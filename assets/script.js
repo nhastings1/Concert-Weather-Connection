69a0d815360d5379c101542c5973c741fbab8512
wtNorb <mollyannmarieh@gmail.com>
2022-07-26 20:58:29 -0500

    html css working

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+document.body.style.overflow = 'hidden';
+
+function scrolldown() {
+    setTimeout(
+      function()
+      {
+        window.scrollTo(0,document.body.scrollHeight);
+        scrolldown();
+      }, 2000
+    )
+  }
+  
+  scrolldown()
